5ec37ef1a52fb0673ba5b031abb681942c5313cc
10sr <8.slashes@gmail.com>
2019-04-09 22:28:47 +0900

    Remove unused comment

--- awk-preview.el
+++ awk-preview.el
@@ -265,8 +265,6 @@ will be used as a awk program to process input."
 
 (defun awk-preview-update-preview ()
   "Update awk-preview."
-  ;; TODO: Do not delete only the target region, instead copy whole contents
-  ;; from source buffer every time
   (interactive)
   (cl-assert awk-preview--env)
   (with-current-buffer (awk-preview--env-program-buffer awk-preview--env)
